46398319f79c89611624c56034d8c5810c3f94b9
YangYangXun <asdf1995905@gmail.com>
2018-08-22 15:08:44 +0800

    post finish

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -1,10 +1,16 @@
-@extends('layouts.admin') 
+@extends('layouts.admin')
 @section('content')
 <h1>Posts Edit</h1>
 
 
 <h2>Edit-> {{$post->title}}</h2>
 
+
+<div class="col-sm-4">
+            <img style="width: 400px" class="image-responsive image-rounded" src="{!! URL::asset($post->photo ? $post->photo->file : 'https://via.placeholder.com/400x400') !!}" >
+</div>
+
+  <div class="col-sm-8">
 {!! Form::open(['method' => 'PATCH', 'action' => ['AdminPostsController@update',$post->id], 'class'=>'mt-5', 'files' => true
 ]) !!} {{ csrf_field() }}
 
@@ -34,5 +40,6 @@
     }}
 
     <button type="submit" class="btn btn-danger btn-block">Delete</button> {!! Form::close() !!}
+</div>
 </div>
 @endsection
